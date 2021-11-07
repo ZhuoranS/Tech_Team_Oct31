f1a16a2dd9ce82d2361d5bc46045000fe13c5a78
Zhuoran (Andrew) Su <zhuoran.su.ufo@gmail.com>
2021-11-07 17:24:02 -0600

    Update

--- Nov7/contracts/SimpleStorage.sol
+++ Nov7/contracts/SimpleStorage.sol
@@ -3,6 +3,10 @@ pragma solidity ^0.5.0;
 contract SimpleStorage {
   string public data;
 
+  constructor() public {
+    data = "Hello Blockchain Group Tech Team";
+  }
+
   function set(string memory _data) public {
     data = _data;
   }
